0840c890c9a075b1b05e9c6a6ddadbed6a1b9565
SankalpUGMan <sankalp_saoji@rediffmail.com>
2017-03-05 15:57:33 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+     document.getElementById("submit_button").onclick = function () {
+        var name = document.getElementById("name_input").value;
+        document.getElementById("name_span").innerHTML = name;
+    }
+});
